65e571d4914c02937e678a9ccfbc0b0b966ce714
jthelin <jthelin@microsoft.com>
2016-01-09 17:11:54 -0800

    Add Test Category attributes + allow running of test cases groups.

--- Test.cmd
+++ Test.cmd
@@ -3,6 +3,17 @@
 
 set PROJ_NAME=ServerHost
 
+@REM Note: Complex values for TEST_CATEGORY can be passed in through environment variable 
+@REM       (as workaround for some limitations of Windows cmd script calling)
+@REM       Command line arguments passed in to this script will override any env variable settings.
+@REM       For example, "Test.cmd BVT" will just run TestCategory = BVT (Build Verification Tests)
+
+if NOT .%1. == .. set TEST_CATEGORY=%1
+
+if NOT .%TEST_CATEGORY%. == .. (
+  @echo Using TestCategory = %TEST_CATEGORY%
+)
+
 SET CONFIGURATION=Release
 
 set USE_BINARIES_DIR=False
@@ -42,6 +53,9 @@ set XUNIT_OPTS=-xml %TEST_RESULTS_DIR%\TestResults.xml
 )
 @Echo Test assemblies = %TESTS%
 
+if NOT .%TEST_CATEGORY%. == .. (
+  set TEST_ARGS= -trait "Category=%TEST_CATEGORY%"
+)
 set TEST_ARGS= %TEST_ARGS% %XUNIT_ARGS%
 
 @Echo ON
