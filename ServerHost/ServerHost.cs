1e12dc649d4eab30b5896716c323c096278873b7
Jorgen Thelin <jthelin@microsoft.com>
2016-05-23 16:10:53 -0700

    Split out ServerHostHandle class into separate source file.

--- ServerHost/ServerHost.cs
+++ ServerHost/ServerHost.cs
@@ -12,26 +12,6 @@ using log4net;
 namespace Server.Host
 {
     /// <summary>
-    /// Data holder class for information related to a hosted in-process test server instance.
-    /// </summary>
-    /// <typeparam name="TServer">Type of the server.</typeparam>
-    public class ServerHostHandle<TServer> 
-    {
-        internal ServerHostHandle()
-        {
-            // Prevent the arbitrary creation of ServerHostHandle objects.
-        }
-
-        /// <summary> The name of this server. </summary>
-        public string ServerName { get; internal set; }
-        /// <summary> Reference to the Server instance in the hosted AppDomain. 
-        /// This should be a <c>MarshalByRefObject</c> to allow cross-domain API calls.</summary>
-        public TServer Server { get; internal set; }
-        /// <summary> Reference to the AppDomain this server is running in. </summary>
-        public AppDomain AppDomain { get; internal set; }
-    }
-
-    /// <summary>
     /// A hosting / test framework class for loading server instances into individual AppDomains in current process.
     /// </summary>
     /// <remarks>Uses <c>log4net</c> for logging. See: http://logging.apache.org/log4net/</remarks>
---
+++ ServerHost/ServerHostHandle.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Jorgen Thelin. All rights reserved.
+// Licensed with Apache 2.0 https://github.com/jthelin/ServerHost/blob/master/LICENSE
+
+using System;
+
+namespace Server.Host
+{
+    /// <summary>
+    /// Data holder class for information related to a hosted in-process test server instance.
+    /// </summary>
+    /// <typeparam name="TServer">Type of the server.</typeparam>
+    public class ServerHostHandle<TServer> 
+    {
+        internal ServerHostHandle()
+        {
+            // Prevent the arbitrary creation of ServerHostHandle objects.
+        }
+
+        /// <summary> The name of this server. </summary>
+        public string ServerName { get; internal set; }
+        /// <summary> Reference to the Server instance in the hosted AppDomain. 
+        /// This should be a <c>MarshalByRefObject</c> to allow cross-domain API calls.</summary>
+        public TServer Server { get; internal set; }
+        /// <summary> Reference to the AppDomain this server is running in. </summary>
+        public AppDomain AppDomain { get; internal set; }
+    }
+}
