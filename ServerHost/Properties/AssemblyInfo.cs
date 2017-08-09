d4588edbdf3ca47ebd6c50e919769cb7fee54219
jthelin <jthelin@microsoft.com>
2017-08-09 09:30:39 -0700

    GitLink - source code location.
    - Use GitLink to link back to GitHub repo for source server code debug.
    
    https://github.com/GitTools/GitLink
    
    - Update copyright date[s]
    
    - Bump package version to `1.1.4` for change tracking.

--- ServerHost/Properties/AssemblyInfo.cs
+++ ServerHost/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jorgen Thelin")]
 [assembly: AssemblyProduct("ServerHost")]
-[assembly: AssemblyCopyright("Copyright © Jorgen Thelin 2015-2016")]
+[assembly: AssemblyCopyright("Copyright © Jorgen Thelin 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
