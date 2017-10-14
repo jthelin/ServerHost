b6fd61fe19f68ec72a6d49a2cf01a5548d8c9efa
Jorgen Thelin <jthelin@microsoft.com>
2017-10-14 00:20:45 -0700

    Xunit 2.3 (#22)
    * Tests -> .NET 4.6.1 [.NET Standard 2.0 compatible]
    
    * xunit test framework v2.3 + FluentAssertions v4.19.4

--- Test.sh
+++ Test.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-mono packages/xunit.runner.console.2.1.0/tools/xunit.console.exe Tests/bin/Debug/ServerHost.Tests.dll
+mono packages/xunit.runner.console.2.3.0/tools/net452/xunit.console.exe Tests/bin/Debug/ServerHost.Tests.dll
