343515ae5cbedf1c76e55830fd188a5772297669
Jorgen Thelin <jthelin@microsoft.com>
2017-10-23 21:35:46 -0700

    Missed removal of old proj file. (#27)

--- ServerHost/ServerHostFixture.cs
+++ ServerHost/ServerHostFixture.cs
@@ -57,6 +57,9 @@ namespace Server.Host
             }
         }
 
+        /// <summary>
+        /// Finalizer function.
+        /// </summary>
         ~ServerHostFixture()
         {
             Dispose(false);
