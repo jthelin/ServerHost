0940b41307bba5f881482deafe86d4f890f949b4
jthelin <jthelin@microsoft.com>
2018-10-15 17:01:02 -0700

    Reset target framework for Tests.Net46 project to 4.6.1
    - net461 is more widely available than net462.
    - It seems .NET v4.6.2 does not get installed on all platforms, eg Win7.

--- Test.sh
+++ Test.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-mono packages/xunit.runner.console.*/tools/net462/xunit.console.exe Tests/Tests.Net46/bin/Debug/ServerHost.Tests.dll
+mono packages/xunit.runner.console.*/tools/net461/xunit.console.exe Tests/Tests.Net46/bin/Debug/ServerHost.Tests.dll
