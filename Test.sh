775764dd1140f580afb8d42cfabe60096a7d3092
Jorgen Thelin <jthelin@microsoft.com>
2020-06-21 11:07:01 -0700

    Fix some shellcheck errors (#93)

--- Test.sh
+++ Test.sh
@@ -1,6 +1,7 @@
-#/usr/bin/bash
+#!/bin/bash
 
 xunit_version=2.4.1
 dotnet_version=net461
 
 mono ~/.nuget/packages/xunit.runner.console/$xunit_version/tools/$dotnet_version/xunit.console.exe Tests/Tests.Net46/bin/Debug/ServerHost.Tests.dll
+
